64459440872ceca449562f026be2889e19056172
tomazv <tomaz.vizintin@gmail.com>
2016-10-25 08:58:15 +0200

    initial commit

---
+++ src/AppLifetime/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using Microsoft.AspNetCore.Hosting;
+
+namespace AppLifetime
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            IApplicationLifetime appLifetime = (IApplicationLifetime)host.Services.GetService(typeof(IApplicationLifetime));
+            appLifetime.ApplicationStarted.Register(() => Log("Application started"));
+            appLifetime.ApplicationStopping.Register(() => Log("Application stopping"));
+            appLifetime.ApplicationStopped.Register(() => Log("Application stopped"));
+
+            Log("Before run");
+            host.Run();
+            Log("After run");
+        }
+
+        private static void Log(string entry)
+        {
+            File.AppendAllText("AppLifetime.log", $"{DateTime.UtcNow} {Process.GetCurrentProcess().Id} {entry}\n");
+        }
+    }
+}
